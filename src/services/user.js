cf20546d58f362d03fce791f200d3488821f8333
RR1335 <odyli@126.com>
2025-03-19 09:29:06 -0400

    index Data Model

--- src/services/user.js
+++ src/services/user.js
@@ -6,6 +6,7 @@
 
 const { User } = require('../db/model/indexModel')
 const { formatUser } = require('./_format')
+const { addFollower } = require('./userRelation')
  
 
 /**
@@ -49,7 +50,12 @@ async function createUser({ userName , password , gender = 3 , nickName }) {
         gender
     })
 
-    return result.dataValues
+    const data = result.dataValues
+
+    // 自己关注自己
+    addFollower(data.id,data.id)
+
+    return data
 }
 
 /**
